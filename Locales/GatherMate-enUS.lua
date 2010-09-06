cb8a86fa850008e04f0e9401a75d33b41a3eb41a
Sal Scotto <sal.scotto@gmail.com>
2010-09-06 16:37:32 -0400

    Added base toc file, copied the descriptions and such from original GatherMate setup some basic localization files, again jsut a copy from gathermate Added a license file and .pkgmeta

---
+++ Locales/GatherMate-enUS.lua
@@ -0,0 +1,16 @@
+﻿-- GatherMate Locale
+-- Please use the Localization App on WoWAce to Update this
+-- http://www.wowace.com/projects/gathermate2/localization/ ;¶
+
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("GatherMate2", "enUS", true, debug)
+
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Options", table-name="L")@
+
+local NL = LibStub("AceLocale-3.0"):NewLocale("GatherMate2Nodes", "enUS", true, debug)
+
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Nodes", table-name="NL")@
---
+++ Locales/GatherMate-koKR.lua
@@ -0,0 +1,13 @@
+﻿-- GatherMate Locale
+-- Please use the Localization App on WoWAce to Update this
+-- http://www.wowace.com/projects/gathermate2/localization/ ;¶
+
+local L = LibStub("AceLocale-3.0"):NewLocale("GatherMate2", "koKR")
+if not L then return end
+
+--@localization(locale="koKR", format="lua_additive_table", namespace="Options", table-name="L", handle-unlocalized="comment")@
+
+local NL = LibStub("AceLocale-3.0"):NewLocale("GatherMate2Nodes", "koKR")
+if not NL then return end
+
+--@localization(locale="koKR", format="lua_additive_table", namespace="Nodes", table-name="NL", handle-unlocalized="comment")@
