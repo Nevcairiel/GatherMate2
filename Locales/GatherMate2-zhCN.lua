1165712a779d533792b813cb522e4fd9b7968448
Sal Scotto <sal.scotto@gmail.com>
2010-10-01 16:19:31 -0400

    more bom fixes

--- Locales/GatherMate2-zhCN.lua
+++ Locales/GatherMate2-zhCN.lua
@@ -1,6 +1,6 @@
-﻿-- GatherMate Locale
+-- GatherMate Locale
 -- Please use the Localization App on WoWAce to Update this
--- http://www.wowace.com/projects/gathermate2/localization/ ;¶
+-- http://www.wowace.com/projects/gathermate2/localization
 
 local L = LibStub("AceLocale-3.0"):NewLocale("GatherMate2", "zhCN")
 if not L then return end
