168b35d155a092385a6008e11f08697def9606ac
Hendrik Leppkes <h.leppkes@gmail.com>
2012-08-27 21:38:03 +0200

    Fix italian locale file and actually load it.

--- Locales/GatherMate2-itIT.lua
+++ Locales/GatherMate2-itIT.lua
@@ -5,9 +5,9 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("GatherMate2", "itIT")
 if not L then return end
 
---@localization(locale="zhTW", format="lua_additive_table", namespace="Options", table-name="L", handle-unlocalized="comment")@
+--@localization(locale="itIT", format="lua_additive_table", namespace="Options", table-name="L", handle-unlocalized="comment")@
 
 local NL = LibStub("AceLocale-3.0"):NewLocale("GatherMate2Nodes", "itIT")
 if not NL then return end
 
---@localization(locale="zhTW", format="lua_additive_table", namespace="Nodes", table-name="NL", handle-unlocalized="comment")@
+--@localization(locale="itIT", format="lua_additive_table", namespace="Nodes", table-name="NL", handle-unlocalized="comment")@
