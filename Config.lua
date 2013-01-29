a3fbd6b7863574bbed9506815fec6228ad2fa88a
Sal Scotto <sal.scotto@gmail.com>
2013-01-29 12:04:13 -0500

    updated auto import to only try get if load was successful, line line out of place

--- Config.lua
+++ Config.lua
@@ -1372,8 +1372,8 @@ function Config:CheckAutoImport()
 			local dataVersion = tonumber(verline:match("%d+"))
 			if dataVersion and dataVersion > v["lastImport"] then
 				local loaded, reason = LoadAddOn(k)
-				local addon = LibStub("AceAddon-3.0"):GetAddon(k)
 				if loaded then
+					local addon = LibStub("AceAddon-3.0"):GetAddon(k)
 					local filter = nil
 					if v.expacOnly then
 						filter = v.expac
