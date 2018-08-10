3f97515ec45aed9a117daba95b0e1b518434d538
Hendrik Leppkes <h.leppkes@gmail.com>
2018-08-10 13:55:51 +0200

    Add separate esMX locale file

--- Locales/GatherMate2-esES.lua
+++ Locales/GatherMate2-esES.lua
@@ -2,12 +2,12 @@
 -- Please use the Localization App on WoWAce to Update this
 -- http://www.wowace.com/projects/gathermate2/localization
 
-local L = LibStub("AceLocale-3.0"):NewLocale("GatherMate2", "esES") or LibStub("AceLocale-3.0"):NewLocale("GatherMate", "esMX")
+local L = LibStub("AceLocale-3.0"):NewLocale("GatherMate2", "esES")
 if not L then return end
 
 --@localization(locale="esES", format="lua_additive_table", namespace="Options", table-name="L", handle-unlocalized="ignore")@
 
-local NL = LibStub("AceLocale-3.0"):NewLocale("GatherMate2Nodes", "esES") or LibStub("AceLocale-3.0"):NewLocale("GatherMateNodes", "esMX")
+local NL = LibStub("AceLocale-3.0"):NewLocale("GatherMate2Nodes", "esES")
 if not NL then return end
 
 --@localization(locale="esES", format="lua_additive_table", namespace="Nodes", table-name="NL", handle-unlocalized="ignore")@
