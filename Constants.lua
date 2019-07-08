88232d3613f0d26cc8f2bbf3c36124a1f18d9e3b
Hendrik Leppkes <h.leppkes@gmail.com>
2019-07-08 09:44:18 +0200

    Add artwork for 8.2 fish schools

--- Constants.lua
+++ Constants.lua
@@ -588,10 +588,10 @@ local node_textures = {
 		[194] = icon_path.."Fish\\slimy_mackerel.tga",
 		[195] = icon_path.."Fish\\tiragarde_perch.tga",
 		[196] = icon_path.."Fish\\utaka.tga",
-		[197] = icon_path.."Fish\\fish_hook.tga", -- 8.2 fish TODO
-		[198] = icon_path.."Fish\\fish_hook.tga", -- 8.2 fish TODO
-		[199] = icon_path.."Fish\\fish_hook.tga", -- 8.2 fish TODO
-		[1101] = icon_path.."Fish\\fish_hook.tga", -- 8.2 fish TODO
+		[197] = icon_path.."Fish\\mauvestinger.tga",
+		[198] = icon_path.."Fish\\viperfish.tga",
+		[199] = icon_path.."Fish\\ionizedminnow.tga",
+		[1101] = icon_path.."Fish\\sentryfish.tga",
 	},
 	["Mining"] = {
 		[201] = icon_path.."Mine\\copper.tga",
