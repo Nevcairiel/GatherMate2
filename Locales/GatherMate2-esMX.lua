8ec521c3d42b5abef2e7cb8ffdc0379122b3ae34
Hendrik Leppkes <h.leppkes@gmail.com>
2020-09-01 11:39:36 +0200

    Update for new project location

--- Locales/GatherMate2-esMX.lua
+++ Locales/GatherMate2-esMX.lua
@@ -1,6 +1,6 @@
 -- GatherMate Locale
 -- Please use the Localization App on WoWAce to Update this
--- http://www.wowace.com/projects/gathermate2/localization/
+-- http://www.wowace.com/projects/gathermate2-classic/localization/
 
 local L = LibStub("AceLocale-3.0"):NewLocale("GatherMate2", "esMX")
 if not L then return end
