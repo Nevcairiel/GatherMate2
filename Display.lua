236ce02dba62db8c0ac4370f1dbe3c15d5d50847
Hendrik Leppkes <h.leppkes@gmail.com>
2022-09-15 14:21:39 +0200

    Add compat for WoW 10.0 tracking API

--- Display.lua
+++ Display.lua
@@ -2,6 +2,10 @@ local GatherMate = LibStub("AceAddon-3.0"):GetAddon("GatherMate2")
 local Display = GatherMate:NewModule("Display","AceEvent-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("GatherMate2")
 
+-- WoW 10.0 tracking API compat
+local GetNumTrackingTypes = GetNumTrackingTypes or C_Minimap.GetNumTrackingTypes
+local GetTrackingInfo = GetTrackingInfo or C_Minimap.GetTrackingInfo
+
 -- Current minimap pin set
 local minimapPins, minimapPinCount = {}, 0
 -- Current worldmap pin set
