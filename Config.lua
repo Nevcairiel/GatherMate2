b4e0d81273198a55aedb230717be59a9d67d9135
Hendrik Leppkes <h.leppkes@gmail.com>
2024-11-28 07:09:22 +0100

    Cleanup some deprecated stuff

--- Config.lua
+++ Config.lua
@@ -1141,29 +1141,13 @@ local faqOptions = {
 local acr = LibStub("AceConfigRegistry-3.0")
 local acd = LibStub("AceConfigDialog-3.0")
 
-local function findPanel(name, parent)
-	for i, button in next, InterfaceOptionsFrameAddOns.buttons do
-		if button.element then
-			if name and button.element.name == name then return button
-			elseif parent and button.element.parent == parent then return button
-			end
-		end
-	end
-end
 function Config:OnInitialize()
 	db = GatherMate.db.profile
 
 	self.importHelper = ImportHelper
 
 	acr:RegisterOptionsTable("GatherMate 2", generalOptions)
-	local options = acd:AddToBlizOptions("GatherMate 2", "GatherMate 2")
-
-	if InterfaceOptionsFrameAddOns then
-		options:HookScript("OnShow", function()
-			local p = findPanel("GatherMate 2")
-			if p and p.element.collapsed then OptionsListButtonToggle_OnClick(p.toggle) end
-		end)
-	end
+	acd:AddToBlizOptions("GatherMate 2", "GatherMate 2")
 
 	acr:RegisterOptionsTable("GM2/Minimap", minimapOptions)
 	acd:AddToBlizOptions("GM2/Minimap", "Minimap", "GatherMate 2")
@@ -1184,11 +1168,7 @@ function Config:OnInitialize()
 	acd:AddToBlizOptions("GM2/FAQ", "FAQ", "GatherMate 2")
 
 	local function openOptions()
-		if Settings and Settings.OpenToCategory then
-			Settings.OpenToCategory("GatherMate 2")
-		else
-			InterfaceOptionsFrame_OpenToCategory("GatherMate 2")
-		end
+		Settings.OpenToCategory("GatherMate 2")
 	end
 
 	SLASH_GatherMate21 = "/gathermate"
@@ -1242,7 +1222,7 @@ function Config:CheckAutoImport()
 		if verline and v["autoImport"] then
 			local dataVersion = tonumber(verline:match("%d+"))
 			if dataVersion and dataVersion > v["lastImport"] then
-				local loaded, reason = LoadAddOn(k)
+				local loaded, reason = C_AddOns.LoadAddOn(k)
 				if loaded then
 					local addon = LibStub("AceAddon-3.0"):GetAddon(k)
 					local filter = nil
